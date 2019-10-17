df97e0d26350790780e8723bfdef451e23812d4a
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-16 22:27:39 -0700

    Renamed windbg.py to debugger.py

--- windbgtool/debugger.py
+++ windbgtool/debugger.py
@@ -744,16 +744,3 @@ class DbgEngine:
 
     def Go(self):
         pykd.go()
-
-if __name__ == '__main__':
-    dbg_engine = DbgEngine()
-    dbg_engine.SetSymbolPath()
-    dbg_engine.EnumerateModules()
-    dbg_engine.LoadSymbols(['kernel32'])
-    def breakpointHandler():
-        print('breakpointHandler:')
-
-    dbg_engine.AddSymbolBP('kernel32','CreateFileA',[])
-    dbg_engine.AddSymbolBP('kernel32','CreateFileW',[])
-    dbg_engine.Go()
-
