db80d972c0a8fd9819b2e43bd862a0180787f107
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-16 22:27:39 -0700

    Renamed windbg.py to debugger.py

---
+++ windbgtool/test_notepad.py
@@ -0,0 +1,13 @@
+import debugger
+
+if __name__ == '__main__':
+    dbg_engine = debugger.DbgEngine()
+    dbg_engine.SetSymbolPath()
+    dbg_engine.EnumerateModules()
+    dbg_engine.LoadSymbols(['kernel32'])
+    def breakpointHandler():
+        print('breakpointHandler:')
+
+    dbg_engine.AddSymbolBP('kernel32','CreateFileA',[])
+    dbg_engine.AddSymbolBP('kernel32','CreateFileW',[])
+    dbg_engine.Go()
