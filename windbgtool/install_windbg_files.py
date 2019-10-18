79b65d8babb0d59a12b8daf5d330c3ac783eed7f
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-18 11:54:25 -0700

    Added support for 32bit installation

--- windbgtool/install_windbg_files.py
+++ windbgtool/install_windbg_files.py
@@ -3,7 +3,10 @@ import platform
 import pprint
 import shutil
 
-(bits, os_name) = platform.architecture()
+if 'PROGRAMFILES(X86)' in os.environ:
+    bits = '64bit'
+else:
+    bits = '32bit'
 
 windbg_folders64 = []
 windbg_folders32 = []
@@ -13,11 +16,11 @@ python_folders32 = []
 
 pykd_folder = r'\Lib\site-packages\pykd'
 if bits == '64bit':
-    windbg_folders64.append(os.environ["ProgramFiles"] + r'\Windows Kits\10\Debuggers\x64')
-    windbg_folders64.append(os.environ["ProgramFiles"] + r'\Debugging Tools for Windows (x64)')
+    windbg_folders64.append(os.environ["ProgramW6432"] + r'\Windows Kits\10\Debuggers\x64')
+    windbg_folders64.append(os.environ["ProgramW6432"] + r'\Debugging Tools for Windows (x64)')
 
     windbg_folders32.append(os.environ["ProgramFiles(x86)"] + r'\Windows Kits\10\Debuggers\x86')
-    windbg_folders32.append(os.environ["ProgramFiles"] + r'\Debugging Tools for Windows (x64)\Wow64')
+    windbg_folders32.append(os.environ["ProgramW6432"] + r'\Debugging Tools for Windows (x64)\Wow64')
 
     python_folders64.append(r'C:\python27-x64' + pykd_folder)
     python_folders64.append(os.environ['LOCALAPPDATA'] + r'\Programs\Python\Python37' + pykd_folder)
@@ -29,44 +32,37 @@ elif bits == '32bit':
     windbg_folders32.append(os.environ["ProgramFiles"])
     python_folders32.append(r'C:\Python27' + pykd_folder)
 
-print('* Copying 64bit files:')
-
-for windbg_folder in windbg_folders64:
-    if not os.path.isdir(windbg_folder):
-        continue
-
-    for python_folder in python_folders64:
-        if not os.path.isdir(python_folder):
-            continue        
-        print('\t'+windbg_folder + ' -> ' + python_folder)
-
-        for filename in ('dbgeng.dll', 'dbghelp.dll', 'DbgModel.dll', 'msdia140.dll', 'srcsrv.dll', 'symsrv.dll'):
-            try:
-                src_filename = os.path.join(windbg_folder, filename)
-                print('Copying ' + src_filename)
-                shutil.copy(src_filename, os.path.join(python_folder, filename))
-            except:
-                pass
-
-        for filename in os.listdir(os.path.join(windbg_folder, 'winext')):
-            if not filename.endswith('.dll'):
-                continue
-
-            try:
-                src_filename = os.path.join(windbg_folder + '\winext', filename)
-                print('Copying ' + src_filename)
-                shutil.copy(src_filename, os.path.join(python_folder, filename))
-            except:
-                pass
-
-print('* Copying 32bit files:')
-
-for windbg_folder in windbg_folders32:
-    if not os.path.isdir(windbg_folder):
-        continue
-
-    for python_folder in python_folders32:
-        if not os.path.isdir(python_folder):
+def CopyFiles(src_folder, dst_folders):
+    for windbg_folder in src_folder:
+        if not os.path.isdir(windbg_folder):
             continue
 
-        print('\t'+windbg_folder + ' -> ' + python_folder)
+        for python_folder in dst_folders:
+            if not os.path.isdir(python_folder):
+                continue        
+            print('\t'+windbg_folder + ' -> ' + python_folder)
+
+            for filename in ('dbgeng.dll', 'dbghelp.dll', 'DbgModel.dll', 'msdia140.dll', 'srcsrv.dll', 'symsrv.dll'):
+                try:
+                    src_filename = os.path.join(windbg_folder, filename)
+                    print('Copying ' + src_filename)
+                    shutil.copy(src_filename, os.path.join(python_folder, filename))
+                except:
+                    pass
+
+            for filename in os.listdir(os.path.join(windbg_folder, 'winext')):
+                if not filename.endswith('.dll'):
+                    continue
+
+                try:
+                    src_filename = os.path.join(windbg_folder + '\winext', filename)
+                    print('Copying ' + src_filename)
+                    shutil.copy(src_filename, os.path.join(python_folder, filename))
+                except:
+                    pass
+
+print('* Copying 64bit files:')
+CopyFiles(windbg_folders64, python_folders64)
+
+print('* Copying 32bit files:')
+CopyFiles(windbg_folders32, python_folders32)
