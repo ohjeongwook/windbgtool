b825555dce250f4f0525a38f6b025cc1f984c050
Jeong Wook Oh <oh.jeongwook@gmail.com>
2020-01-08 14:20:37 -0800

    Addeded dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ohjeongwook/windbgtool",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pykd',
+        'capstone',
+        'pyvex',
+        'archinfo',
+        'cffi',
+        'idatool-ohjeongwook @ git+https://github.com/ohjeongwook/idatool@v1.0#egg=idatool-ohjeongwook',
+    ],    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
