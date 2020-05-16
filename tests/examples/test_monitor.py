77993237235e04e3ccb2fb9b70fe271050e1790c
Jeong Wook Oh <oh.jeongwook@gmail.com>
2020-05-15 18:45:48 -0700

    Added more test apis

--- tests/examples/test_monitor.py
+++ tests/examples/test_monitor.py
@@ -13,12 +13,18 @@ if __name__ == '__main__':
     debugger.load_symbols(['kernel32'])
 
     monitor_breakpoints = windbgtool.monitor.Breakpoints()
-    monitor_breakpoints.add('kernel32!CreateFileA')
-    monitor_breakpoints.add('kernel32!CreateFileW')
-    monitor_breakpoints.add('kernelbase!CreateFileA')
-    monitor_breakpoints.add('kernelbase!CreateFileW')
-    monitor_breakpoints.add('kernel32!WriteFile')
-    monitor_breakpoints.add('kernelbase!WriteFile')
+    api_list = [
+        'kernel32!CreateFileA', 'kernel32!CreateFileW', 'KERNELBASE!CreateFileA', 'KERNELBASE!CreateFileW',
+        'kernel32!WriteFile', 'KERNELBASE!WriteFile',
+        "KERNELBASE!RegQueryValueExA", "KERNELBASE!RegQueryValueExW", "KERNELBASE!RegOpenKeyExA", "KERNELBASE!RegOpenKeyExW", "KERNELBASE!RegCreateKeyExA", 
+        "KERNELBASE!RegCreateKeyExW", "KERNELBASE!RegGetValueA", "KERNELBASE!RegGetValueW", "KERNELBASE!RegCloseKey", "KERNELBASE!RegSetValueExW", "KERNELBASE!RegSetValueExA",
+        "KERNELBASE!RegSetKeyValueA", "KERNELBASE!RegSetKeyValueW", "KERNELBASE!RegEnumKeyExA", "KERNELBASE!RegEnumKeyExW", "KERNELBASE!RegGetKeySecurity",
+        "KERNELBASE!RegQueryInfoKeyA", "KERNELBASE!RegQueryInfoKeyW", "KERNELBASE!RegDeleteKeyValueW", "KERNELBASE!RegDeleteKeyValueA", "KERNELBASE!RegDeleteKeyExA",
+        "KERNELBASE!RegDeleteKeyExW", "KERNELBASE!RegDeleteValueA", "KERNELBASE!RegDeleteValueW", "KERNELBASE!RegEnumValueA", "KERNELBASE!RegEnumValueW", "KERNELBASE!RegDeleteTreeA", "KERNELBASE!RegDeleteTreeW"
+    ]
+
+    for api in api_list:
+        monitor_breakpoints.add(api)
     
     debugger.go()
 
