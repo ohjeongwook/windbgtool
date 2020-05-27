6b3e1f1dff16a966831b6984de2600f8246ef221
Matt Oh <oh.jeongwook@gmail.com>
2020-05-27 12:54:49 -0700

    Added windbgtool/windows_api.json in json

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ohjeongwook/windbgtool",
     packages=setuptools.find_packages(),
+    package_data={'': ['windbgtool/windows_api.json']},
+    include_package_data=True, 
     install_requires=[
         'pykd',
         'capstone',
