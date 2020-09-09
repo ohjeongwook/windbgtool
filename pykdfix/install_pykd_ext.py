b57ff70c46c31fbfedc797c7e11ec3d3a00505e5
Matt Oh <oh.jeongwook@gmail.com>
2020-09-09 10:10:15 -0700

    added latest package

--- pykdfix/install_pykd_ext.py
+++ pykdfix/install_pykd_ext.py
@@ -3,12 +3,13 @@ import shutil
 import urllib.request
 import zipfile
 
-pykd_ext_url = r'https://githomelab.ru/pykd/pykd-ext/wikis/uploads/4a0394c82eac0804f145741bd9398050/pykd_ext_2.0.0.22.zip'
-pykd_ext_file = 'pykd_ext_2.0.0.22.zip'
+pykd_ext_file = 'pykd_ext_2.0.0.24'
+pykd_ext_zip_filename = pykd_ext_file + '.zip'
+pykd_ext_url = r'https://githomelab.ru/pykd/pykd-ext/-/wikis/uploads/0bc82100609d24a8fcd1604203e782a6/' + pykd_ext_zip_filename
 pykd_ext_folder = '.'
 
-urllib.request.urlretrieve(pykd_ext_url, pykd_ext_file)
-with zipfile.ZipFile(pykd_ext_file, 'r') as zip_ref:
+urllib.request.urlretrieve(pykd_ext_url, pykd_ext_zip_filename)
+with zipfile.ZipFile(pykd_ext_zip_filename, 'r') as zip_ref:
     zip_ref.extractall(pykd_ext_folder)
 
 if 'PROGRAMFILES(X86)' in os.environ:
@@ -35,7 +36,7 @@ for windbg_folder in windbg_folders64:
     if not os.path.isdir(windbg_folder):
         continue
 
-    src = r'pykd_ext_2.0.0.22\x64\pykd.dll'
+    src = pykd_ext_file + r'\x64\pykd.dll'
     dst = windbg_folder + '\winext'
     print('Copying "%s" -> "%s"' % (src, dst))
     shutil.copy(src, dst)
@@ -44,8 +45,7 @@ for windbg_folder in windbg_folders32:
     if not os.path.isdir(windbg_folder):
         continue
 
-    src = r'pykd_ext_2.0.0.22\x86\pykd.dll'
+    src = pykd_ext_file + r'\x86\pykd.dll'
     dst = windbg_folder + '\winext'
     print('Copying "%s" -> "%s"' % (src, dst))
     shutil.copy(src, dst)
-
